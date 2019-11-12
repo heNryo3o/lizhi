a66e4776b5293997552bae088c6377d60a4214b9
henryo3o <18661139072@163.com>
2019-11-12 11:10:00 +0800

    1

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,12 @@
 <footer>
-    <div class="mdui-container">
+    <div class="mdui-container" style="text-align: center;">
         <div class="footer-copyright">Copyright © 2019 <a href="{{ route('/') }}">励志语录</a> All rights reserved.</div>
         <div class="footer-copyright">友情链接：
             <a href="https://www.5aizhuanqian.com" target="_blank" class="mdui-m-r-1">我爱赚钱网</a>
             <a href="https://www.yuyinuo.cn" target="_blank">于高衡的博客</a>
+        </div>
+        <div class="footer-copyright">
+            <a href="https://www.lzyl365.com/sitemap.xml" target="_blank">网站地图</a>
         </div>
     </div>
 </footer>
