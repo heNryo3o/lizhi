7f06820e4247640412e1a573fe0ea80e11ac934b
henryo3o <18661139072@163.com>
2019-11-30 16:05:03 +0800

    init

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,6 @@
 <footer>
     <div class="mdui-container text-center text-center">
+        <div class="footer-copyright">励志语录365网致力于分享励志名言,名人名言,励志电影,励志歌曲,经典语录,经典语句相关内容。以成为中国最好的分享励志语句的网站为目标，不断优化使用体验。励志，从这里开始！</div>
         <div class="footer-copyright">Copyright © 2019 <a href="{{ route('/') }}">励志语录365网</a> All rights reserved.</div>
         <div class="footer-copyright">友情链接：
             <a href="https://www.5aizhuanqian.com" target="_blank" class="mdui-m-r-1">我爱赚钱网</a>
