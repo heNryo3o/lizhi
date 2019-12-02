9be31a87573cd40a421991c0a5b708cb66aef085
henryo3o <18661139072@163.com>
2019-12-02 21:04:17 +0800

    init

--- config/app.php
+++ config/app.php
@@ -21,6 +21,29 @@ return [
 
     'youdao_secret' => env('YOUDAO_SECRET'),
 
+    'friend_links' => [
+        [
+            'name' => '我爱赚钱网',
+            'url' => 'https://www.5aizhuanqian.com'
+        ],
+        [
+            'name' => '四平办公伙伴',
+            'url' => 'https://www.spxroffice.com'
+        ],
+        [
+            'name' => '于高衡的博客',
+            'url' => 'https://www.yuyinuo.cn'
+        ],
+        [
+            'name' => '弹簧减震器',
+            'url' => 'http://www.shanghaijzq.com'
+        ],
+        [
+            'name' => '橡胶软接头',
+            'url' => 'http://www.songxiajz.com'
+        ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
